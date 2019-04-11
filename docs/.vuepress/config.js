4c0aa052abad5de7d43710dce84b4584906cd283
Cale Dunlap <cdunlap@columnfivemedia.com>
2019-04-10 23:50:01 -0700

    Initial setup

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'C5 Netlify CMS',
+  description: 'C5 Netlify CMS',
+  themeConfig: {
+    docsDir: 'docs',
+    sidebar: [
+      '/',
+      '/welcome',
+      '/test'
+    ],
+    nav: [{
+      text: 'Admin',
+      link: '/admin/#/'
+    }]
+  }
+}
